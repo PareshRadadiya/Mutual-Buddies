3f72bfc203f539f717e843785d8441023b892893
Paresh Radadiya <pareshradadiya@hotmail.com>
2015-07-30 03:09:33 +0530

    Don't show mutual friends link for the current user

--- includes/bp-mutual-friends-functions.php
+++ includes/bp-mutual-friends-functions.php
@@ -11,17 +11,20 @@ function bp_mutual_friends_user_filter( $arg ) {
 
 	if ( bp_is_mutual_friends_component() ) {
 		$arg['exclude'] = bp_uncommon_friends();
+		$arg['user_id'] = get_current_user_id();
 		//$arg['per_page'] = apply_filters( 'bp_mutual_friends_per_page', 0 );
 	} else if ( defined( 'DOING_AJAX' )
 	            && isset( $_REQUEST['user_id'] )
 	            && 'mutual_friends_dialog' === $_REQUEST['action']
 	) {
 
+
 		$arg['per_page'] = apply_filters( 'bp_mutual_friends_per_page', 0 );
 		$arg['exclude']  = bp_uncommon_friends( intval( $_REQUEST['user_id'] ) );
+
+		$arg['user_id'] = get_current_user_id();
 	}
 
-	$arg['user_id'] = get_current_user_id();
 
 	return $arg;
 }
@@ -93,6 +96,11 @@ function bp_directory_mutual_friends_count() {
 	}
 
 	$mutual_friends_count = bp_mutual_friend_total_count( $members_template->member->ID );
+
+	if ( get_current_user_id() == $members_template->member->ID ) {
+		return;
+	}
+
 	?>
 	<div class="item-meta">
 		<a href="" data-user-id="<?php echo $members_template->member->ID; ?>" class="mutual-friends">
