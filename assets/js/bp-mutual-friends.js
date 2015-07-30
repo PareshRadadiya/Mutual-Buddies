3ba8546d8cc4247af9812f28a12fea5aa229e905
Paresh Radadiya <pareshradadiya@hotmail.com>
2015-07-31 01:27:59 +0530

    Magnific popup pagination not working fix

--- assets/js/bp-mutual-friends.js
+++ assets/js/bp-mutual-friends.js
@@ -4,6 +4,35 @@ var mutual_friends;
     mutual_friends = {
 
         init: function() {
+            mutual_friends.init_maginific_popup();
+            $('body').on( 'click', 'a.mutual-friends', mutual_friends.fetch_mutual_friend );
+
+            $( document ).ajaxComplete( mutual_friends.rebind_magnific_popup );
+        },
+
+        fetch_mutual_friend: function( e ) {
+
+            $element = $(this);
+
+            e.preventDefault();
+
+            $('div.bmf-white-popup').html('<div class="bmf-spinner"></div>');
+
+            var user_id = $element.data('user-id');
+            var send_data = {
+                action: 'mutual_friends_dialog',
+                user_id: user_id
+            };
+
+            $.post( ajaxurl, send_data, function( response ) {
+
+                $('div.bmf-white-popup').find("div.bmf-spinner").remove();
+                $('div.bmf-white-popup').append( '<button title="Close (Esc)" type="button" class="mfp-close">×</button>'+response );
+                $('div.bmf-white-popup').perfectScrollbar();
+            });
+        },
+
+        init_maginific_popup: function () {
 
             $('a.mutual-friends').magnificPopup({
                 items: {
@@ -13,36 +42,32 @@ var mutual_friends;
                 showCloseBtn: true,
                 closeBtnInside:true
             });
-
-            $('a.mutual-friends').live( 'click', mutual_friends.fetch_mutual_friend );
-
         },
 
-        fetch_mutual_friend: function( e ) {
+        rebind_magnific_popup: function( event, xhr, settings ) {
+            var url = settings.data;
+            var action = parameter_value( url, 'action' );
 
-            e.preventDefault();
+            if ( 'members_filter' == action ) {
+                var timer = setTimeout( function() {
 
-            $('div.bmf-white-popup').html('<div class="bmf-spinner"></div>');
+                    $element = $('#buddypress').find('a.mutual-friends');
 
-            $element = $(this);
-
-            var user_id = $element.data('user-id');
-            var send_data = {
-                action: 'mutual_friends_dialog',
-                user_id: user_id
-            };
-
-            $.get( ajaxurl, send_data, function( response ) {
-
-                $('div.bmf-white-popup').find("div.bmf-spinner").remove();
-                $('div.bmf-white-popup').append( '<button title="Close (Esc)" type="button" class="mfp-close">×</button>'+response );
-                $('div.bmf-white-popup').perfectScrollbar();
-            });
+                    if ( 'undefined' != typeof $element ) {
+                        mutual_friends.init_maginific_popup();
+                        clearInterval( timer );
+                        return false;
+                    }
+                }, 1000);
+            }
         }
 
     };
 
-
-
     $( document).ready( function() { mutual_friends.init() });
 })(jQuery);
+
+function parameter_value( url, name ) {
+    var result = new RegExp( name + "=([^&]*)", "i").exec(url);
+    return result && unescape(result[1]) || "";
+}
